05980ac9a3821d6f6a0b1148514ab600975cf180
Mark Topper <mail@webman.io>
2015-02-20 10:06:55 +0100

    Update CutyCapt.php

--- src/MarkTopper/CutyCapt/CutyCapt.php
+++ src/MarkTopper/CutyCapt/CutyCapt.php
@@ -2,9 +2,11 @@
 namespace MarkTopper\CutyCapy;
 
 use MarkTopper\CutyCapt\Command\Generator as CommandGenerator;
+
 class CutyCapt {
   
   protected $CommandGenerator;
+  protected static $debug_mode;
   
   function __construct()
   {
@@ -13,13 +15,14 @@ class CutyCapt {
   
   protected function getter($key)
   {
-    $this->CommandGenerator->getter($key);
-    return $this;
+  	$method = 'get' . ucfirst(camel_case($key));
+    return $this->CommandGenerator->$method();
   }
   
   protected function setter($key, $value)
   {
-    $this->CommandGenerator->setter($key, $value);
+  	$method = 'set' . ucfirst(camel_case($key));
+    $this->CommandGenerator->$method($value);
     return $this;
   }
   
@@ -30,25 +33,54 @@ class CutyCapt {
   
   public function output($value = null)
   {
-    if ($this->value) $this->CommandGenerator->setOutput($value);
-    $this->run();
+    if ($value) $this->CommandGenerator->setOut($value);
+    return $this->run();
   }
   
   public function show()
   {
     $this->output();
-    return $this->CommandGenerator->getOutput();
+    
+    $out = $this->getOut();
+    $base = CommandGenerator::$base_output;
+    
+    $file = $base . $out;
+    
+    header("Content-type: image/png");
+	readfile("$file");
+	exit;
+  }
+  
+  public function run()
+  {
+	  $command = $this->CommandGenerator->getCommand();
+	  exec($command);
+	  return $this;
+  }
+  
+  public static function debug($mode = 0)
+  {
+	  self::$debug_mode = $mode;
+	  CommandGenerator::debug($mode);
   }
   
   public function __call($method, $parameters)
   {
-    swtich(substr(snake_case($method), 0, 4))
+    switch(substr(snake_case($method), 0, 4))
     {
       case 'get_': return $this->getter(substr(snake_case($method), 4)); break;
-      case 'set_': return $this->getter(substr(snake_case($method), 4), isset($parameters[0]) ? $parameters[0] : null); break;
+      case 'set_': return $this->setter(substr(snake_case($method), 4), isset($parameters[0]) ? $parameters[0] : null); break;
+      default: throw new \Exception('Method "' . $method . '" not found'); break;
     }
-    
-    throw new Exception('Method " . $method . " not found');
+  }
+  
+  public static function __callStatic($method, $parameters)
+  {
+    switch(snake_case($method))
+    {
+      case 'set_base_output': CommandGenerator::$base_output = isset($parameters[0]) ? $parameters[0] : null; break;
+      default: throw new \Exception('Method "' . $method . '" not found'); break;
+    }
   }
   
 }
